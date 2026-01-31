7f742c4e0deeb02aa3b933f5d62673013716982f
Mazhar Shaikh <mazhar.shaikhh17@gmail.com>
2026-01-31 12:27:42 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -96,5 +96,43 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+#datasource for amazon linux
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"] # Specifies aws root as the owner
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"] # Filters names starting with the pattern
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+#EC2 creation
+
+resource "aws_instance" "my_amlinux" {
+  ami                         = data.aws_ami.amazon_linux_2023.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.my_main_sg.id]
+
+  tags = {
+    Name = "Main_Server"
+  }
+
+}
+
+
+
+
 
 
